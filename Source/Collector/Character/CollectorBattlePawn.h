21e2632709de704afd67941cbf56d4244cebaf04
MrSbkz <48620291+MrSbkz@users.noreply.github.com>
2025-10-28 18:54:23 +0100

    Create BattleTable blueprint (#2)

--- Source/Collector/Character/CollectorBattlePawn.h
+++ Source/Collector/Character/CollectorBattlePawn.h
@@ -12,18 +12,5 @@ class COLLECTOR_API ACollectorBattlePawn : public APawn
 	GENERATED_BODY()
 
 public:
-	// Sets default values for this pawn's properties
 	ACollectorBattlePawn();
-
-protected:
-	// Called when the game starts or when spawned
-	virtual void BeginPlay() override;
-
-public:	
-	// Called every frame
-	virtual void Tick(float DeltaTime) override;
-
-	// Called to bind functionality to input
-	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
-
 };
